476beaac142fe43f5aa938805e1d4371d50db8ef
John Touron <johntouron@gmail.com>
2014-07-03 16:43:33 +0100

    Added Project Euler problem 31

--- ProjectEuler/Problem31.hs
+++ ProjectEuler/Problem31.hs
@@ -1,10 +1,12 @@
---import qualified Data.Array.Unboxed as A
-import qualified Data.HashMap.Strict as M
+import qualified Data.HashMap.Lazy as LM
+import qualified Data.IntMap as IM
+import           Data.List (sort)
 
 data GBCoin = OnePence | TwoPence | FivePence | TenPence
             | TwentyPence | FiftyPence | OnePound | TwoPound
             deriving (Eq,Ord,Show)
 
+solve :: Int
 solve = length $ coinCombos
   where
     coinCombos = [ coins
@@ -42,32 +44,45 @@ coinWorth FiftyPence = 50
 coinWorth OnePound = 100
 coinWorth TwoPound = 200
 
-main :: IO ()
-main = print solve
-
 -- much faster way
+-- based on http://users.softlab.ntua.gr/~ttsiod/euler31.html
 
-solveDynamic target coins = table
+-- Thinking the same way:
+
+-- Target in cents	only 1p	<= 2p	<= 5p	<= 10p	<= 20p	<= 50p	<= 100p	<= 200p
+-- 1	1	1	1	1	1	1	1	1
+-- 2	1	2	2	2	2	2	2	2
+-- 3	1	2	2	2	2	2	2	2
+-- 4	1	3	3	3	3	3	3	3
+-- 5	1	3	4	4	4	4	4	4
+
+-- 3 cents can be formed as (1p+1p+1p,1p+2p), so starting from column "<=2p", the number is 2
+-- 4 cents can be formed as (4x1p, 2x1p+1x2p, 2x2p), so starting from column "<=2p", the number is 3
+-- 5 cents can be formed as (5x1p, 3x1p+1x2p, 1x1p+2x2p, 1x5p), so starting from column "<=5p", the number is 4
+-- etc...
+
+-- So, now that we've done this process manually, what do we notice?
+-- We notice that when forming the values in a line, the first column is always 1. There is only one way to form any target N, if you just use 1p coins: Nx1p.
+-- We also notice that when filling up a cell, we check to see if the corresponding coin "fits" in. If it doesn't, we just copy the value of the cell on the left - i.e. the number of ways to form a target sum doesn't change, since we can't use this column's coin.
+-- If the coin *does* fit, however, we then form a number by adding two things: (a) the number of ways we can form the target WITHOUT using the coin (which is on the cell on our left) plus (b) the number of ways we can form the target-columnCoin, i.e. the number of ways to form the remainder, if we subtract (i.e. use) the coin from our target.
+
+-- For example, on line 5 (target: 5 cents) the highlighted cell of column "<=2p" is formed as 1+2:
+-- There is 1 way to form a target of 5 cents using only 1p coins (the cell on the left has value 1)
+-- If we use a coin of 2p, then the remainder is target-2p=5p-2p=3p, which we can lookup above, on line 3, and see that there are 2 ways to reach it, using "<=2p".
+
+-- This lookup is key - we basically reuse previous calculations with a single lookup.
+
+solveDynamic :: [Int] -> Int -> Int
+solveDynamic coins target = go target $ maximum coins
   where
-    go 0 _ = 1
+    -- go => max coin value available, target value
+    go _ 1 = 1
     go 1 _ = 1
     go t c = if c > t
-               then table M.! ()
-    table :: M.HashMap (Int,Int) Int
-    table = M.fromList [((t,c), go t c) | t <- [0..target], c <- coins]
+             then table LM.! (t, prevCoin IM.! c)
+             else table LM.! (t, prevCoin IM.! c) + table LM.! (t-c,c)
+    table = LM.fromList [((t,c), go t c) | t <- [0..target], c <- coins]
+    prevCoin = IM.fromList $ let coins' = reverse $ sort coins in zip coins' $ tail coins'
 
---solveDynamic :: Int -> [Int] -> _
--- solveDynamic target coins = (solTable,coinTable)
---   where
---     go 0 _ = 1
---     go 1 _ = 1
---     go t c = if c > t
---              then solTable A.! (c-1)
---              else solTable A.! (c-1)
---     solTable :: A.UArray (Int,Int) Int
---     solTable = A.listArray ((0,1), (target,length coins)) [go t c | t <- [0..target], c <- coins]
---     coinTable :: A.UArray Int Int
---     coinTable = A.listArray (1,length coins) coins
---     coinValue ci = coinTable A.! ci
-
-dynamicSolution = solveDynamic 10 [1,2,5,10,20,50,100,200]
+main :: IO ()
+main = print $ solveDynamic [1,2,5,10,20,50,100,200] 200
